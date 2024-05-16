205f70a362e83883bb4a9ed374d2cf2f13f052fc
Victor Vaz <triceps_mettle_0f@icloud.com>
2024-05-16 15:30:42 -0300

    Initial Commit

---
+++ ConversorDeMoedas/ConversorDeMoedasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversorDeMoedasApp.swift
+//  ConversorDeMoedas
+//
+//  Created by Victor Vaz de Oliveira on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ConversorDeMoedasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
