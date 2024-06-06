55ec7dffd43259b478c94387b0b993d55064adb0
Victor Vaz <triceps_mettle_0f@icloud.com>
2024-06-06 15:21:14 -0300

    Currency seleciona e seus valores agora são salvos automaticamente se o usuário fechar o app.

---
+++ ConversorDeMoedas/CurrencyTip.swift
@@ -0,0 +1,8 @@
+//
+//  CurrencyTip.swift
+//  ConversorDeMoedas
+//
+//  Created by Victor Vaz de Oliveira on 30/05/24.
+//
+
+import Foundation
---
+++ ConversorDeMoedas/userDefaultsExt.swift
@@ -0,0 +1,8 @@
+//
+//  userDefaultsExt.swift
+//  ConversorDeMoedas
+//
+//  Created by Victor Vaz de Oliveira on 03/06/24.
+//
+
+import Foundation
